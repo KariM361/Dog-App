de7c244f763a77411c5274b5f592ce0ac187504d
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-04-14 09:20:14 +0200

    v9

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW failed:', err))
+  })
+}
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
